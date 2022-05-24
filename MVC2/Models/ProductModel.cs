7f649bb21dd09f9ea5b2f09539f7b0ff95ee47bf
pduykhanh <pduykhanh@vnpt.vn>
2022-05-24 16:35:16 +0700

    add all

---
+++ MVC2/Models/ProductModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MVC2.Models
+{
+    internal class ProductModel
+    {
+        public ProductModel()
+        {
+        }
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ MVC2/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $(window).on('scroll', function () {
+        if (Math.round($(window).scrollTop()) > 50) {
+            $('.navbar').addClass('scrolled');
+        } else {
+            $('.navbar').removeClass('scrolled');
+        }
+    });
+});
